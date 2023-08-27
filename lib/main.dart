03b2de38d2ebe50626bfb0a5c30fa252e023f8a4
克里斯克語風 <cartons-mush.0z@icloud.com>
2023-08-27 17:12:43 +0800

    首次提交。

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget
+{
+  final String text;
+  const CustomButton(this.text);
+  Widget build(context)
+  {
+    return Button(
+      child: Text(text)
+    );
+  }
+}
+
+void main() {
+  print('Hello, Dart!');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n
